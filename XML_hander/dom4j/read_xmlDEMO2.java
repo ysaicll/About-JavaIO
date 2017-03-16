b408e11f61f1b3e8d3f9ab341962c24a4347e5bd
Leo Sheng <ysaicll@sina.com>
2017-03-16 15:19:19 +0800

    dom4j

---
+++ XML_hander/dom4j/read_xmlDEMO2.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.util.Iterator;
+import org.dom4j.Attribute;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+public class Demo {
+ public static void main(String[] args) throws Exception {
+  SAXReader reader = new SAXReader();
+  Document document = reader.read(new File("books.xml"));
+  Element root = document.getRootElement();
+
+  Iterator it = root.elementIterator();
+  while (it.hasNext()) {
+   Element element = (Element) it.next();
+
+   //未知属性名称情况下
+   /*Iterator attrIt = element.attributeIterator();
+   while (attrIt.hasNext()) {
+    Attribute a  = (Attribute) attrIt.next();
+    System.out.println(a.getValue());
+   }*/
+
+   //已知属性名称情况下
+   System.out.println("id: " + element.attributeValue("id"));
+
+   //未知元素名情况下
+   /*Iterator eleIt = element.elementIterator();
+   while (eleIt.hasNext()) {
+    Element e = (Element) eleIt.next();
+    System.out.println(e.getName() + ": " + e.getText());
+   }
+   System.out.println();*/
+
+   //已知元素名情况下
+   System.out.println("title: " + element.elementText("title"));
+   System.out.println("author: " + element.elementText("author"));
+   System.out.println();
+  }
+ }
+}
